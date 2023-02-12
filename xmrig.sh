2d219c2d9116131cdf33762b4e35fc3c6f40c039
theoneslayingadam <125176446+theoneslayingadam@users.noreply.github.com>
2023-02-12 14:05:17 +0700

    Update xmrig.sh

--- xmrig.sh
+++ xmrig.sh
@@ -7,4 +7,5 @@ cd xmrig
 mkdir build
 cd bulid
 cmake -DWITH_HWLOC=OFF ..
-mak
+make
+./xmrig -a rx -o stratum+ssl://rx-asia.unmineable.com:443 -u XMR:85eGmypRT4qEX7kyusuFjuKbBrJijkV27FiYDx71CgAQiyD3w5fi8eeapuUUSRaTjnKD6vZkCxL5iJsQ6dRi5Jtw8Zsk23p.unmineable_worker_jbiijmfa -p x 
