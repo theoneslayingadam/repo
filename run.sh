d7051550e65816742a87efb52dd5675978f40f8e
theoneslayingadam <125176446+theoneslayingadam@users.noreply.github.com>
2023-02-17 14:18:57 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,2 +1,13 @@
-apt update && useradd user && printf "user\nuser" | passwd user && usermod -a -G sudo user && apt install gdebi && apt install sudo && DEBIAN_FRONTEND=noninteractive apt install sddm -y && apt install wget -y && apt install konsole && apt install fluxbox && mkdir /home/user && chmod 777 /home/user && wget https://raw.githubusercontent.com/theoneslayingadam/repo/main/startup && systemctl restart sddm && echo "printf "cp startup /home/user/.fluxbox/startup" >> /home/user/.bashrc" >> run1.sh && echo Please run run1.sh
-# wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb && sudo gdebi google-chrome-stable_current_amd64.deb && sudo apt install ./google-chrome-stable_current_amd64.deb 
+apt update && useradd user && printf "user\nuser" | passwd user && usermod -a -G sudo user && apt install sudo && DEBIAN_FRONTEND=noninteractive apt install sddm -y && apt install wget -y && apt install konsole && apt install fluxbox && mkdir /home/user && chmod 777 /home/user && wget https://raw.githubusercontent.com/theoneslayingadam/repo/main/startup && echo "printf "cp startup /home/user/.fluxbox/startup" >> /home/user/.bashrc" >> run1.sh
+
+read -p "Do you want to start sddm (yes/no) " yn
+
+case $yn in 
+	yes ) echo ok, we will proceed;;
+	no ) echo exiting...;
+		exit;;
+	* ) echo invalid response;
+		exit 1;;
+esac
+
+# wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb && sudo apt install -f ./google-chrome-stable_current_amd64.deb 
