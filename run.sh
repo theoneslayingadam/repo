306c51055b936571e5dd031212820224f349c5f7
theoneslayingadam <125176446+theoneslayingadam@users.noreply.github.com>
2023-02-17 14:27:43 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-apt update && useradd user && printf "user\nuser" | passwd user && usermod -a -G sudo user && apt install sudo && DEBIAN_FRONTEND=noninteractive apt install sddm -y && apt install wget -y && apt install konsole && apt install fluxbox && mkdir /home/user && chmod 777 /home/user && wget https://raw.githubusercontent.com/theoneslayingadam/repo/main/startup && echo "printf "cp startup /home/user/.fluxbox/startup" >> /home/user/.bashrc" >> run1.sh
+apt update && apt install sudo && DEBIAN_FRONTEND=noninteractive apt install sddm -y && apt install konsole && apt install fluxbox && wget https://raw.githubusercontent.com/theoneslayingadam/repo/main/startup && cp startup /home/user/.fluxbox/startup" >> /home/user/.bashrc
 
 read -p "Do you want to start sddm (yes/no) " yn
 
