5ecd39d5e403eb7167b153959e5535a4c2d654c0
theoneslayingadam <125176446+theoneslayingadam@users.noreply.github.com>
2023-02-17 15:06:00 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
-apt update && apt install sudo && DEBIAN_FRONTEND=noninteractive apt install sddm -y && apt install konsole && apt install fluxbox && wget https://raw.githubusercontent.com/theoneslayingadam/repo/main/startup && cp startup /home/user/.fluxbox/startup" >> /home/user/.bashrc
+apt update && apt install sudo && DEBIAN_FRONTEND=noninteractive apt install sddm -y && apt install konsole && apt install fluxbox && wget https://raw.githubusercontent.com/theoneslayingadam/repo/main/startup && cp startup /home/user/.fluxbox/startup >> /home/user/.bashrc
 
 # wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb && sudo apt install -f ./google-chrome-stable_current_amd64.deb 
