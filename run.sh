258e71e83635c84d8acce8e666c823dbf4df6416
theoneslayingadam <125176446+theoneslayingadam@users.noreply.github.com>
2023-02-18 19:47:14 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-apt update && apt install sudo && DEBIAN_FRONTEND=noninteractive -y apt install sddm && apt install -y konsole && apt install -y fluxbox && wget https://raw.githubusercontent.com/theoneslayingadam/repo/main/startup && cp startup /home/user/.fluxbox/startup >> /home/user/.bashrc
+apt update && apt install sudo && DEBIAN_FRONTEND=noninteractive -y apt install sddm && apt install -y konsole && apt install -y fluxbox && wget https://raw.githubusercontent.com/theoneslayingadam/repo/main/startup && cp startup /home/user/.fluxbox/startup
 add-apt-repository ppa:mozillateam/ppa
 echo '
 Package: *
@@ -10,4 +10,7 @@ Pin: version 1:1snap1-0ubuntu2
 Pin-Priority: -1
 ' | sudo tee /etc/apt/preferences.d/mozilla-firefox
 apt install firefox
+
+# (optional)
+# do not install if you are using docker
 # wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb && sudo apt install -f ./google-chrome-stable_current_amd64.deb 
