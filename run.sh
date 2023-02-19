4508ae840d331d7458d431ab5a29513217440faa
theoneslayingadam <125176446+theoneslayingadam@users.noreply.github.com>
2023-02-19 08:27:51 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-apt update && apt install sudo && DEBIAN_FRONTEND=noninteractive apt install sddm -y && apt install -y software-properties-common && apt install -y konsole && apt install -y fluxbox && wget https://raw.githubusercontent.com/theoneslayingadam/repo/main/startup && cp startup /home/user/.fluxbox/startup
+apt update && apt install sudo && DEBIAN_FRONTEND=noninteractive apt install sddm -y && apt install -y software-properties-common && apt install -y konsole && apt install -y fluxbox && wget https://raw.githubusercontent.com/theoneslayingadam/repo/main/startup && mkdir /home/user/.fluxbox && cp startup /home/user/.fluxbox/startup
 add-apt-repository ppa:mozillateam/ppa
 echo '
 Package: *
@@ -9,7 +9,7 @@ Package: firefox
 Pin: version 1:1snap1-0ubuntu2
 Pin-Priority: -1
 ' | sudo tee /etc/apt/preferences.d/mozilla-firefox
-apt install firefox
+apt install -y firefox
 
 # (optional)
 # do not install if you are using docker
