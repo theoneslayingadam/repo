bcde5c19e4aacfadf73240a9b2426abb4c4e23aa
theoneslayingadam <125176446+theoneslayingadam@users.noreply.github.com>
2023-02-23 17:27:02 +0700

    Update test.py

--- test.py
+++ test.py
@@ -6,7 +6,7 @@ nest_asyncio.apply()
 intents = discord.Intents().default()
 intents.message_content = True
 client = discord.Client(intents=intents)
-token = INSERTTOKENHERE
+token = "INSERTTOKENHERE"
 n = 1
 i = 10
 async def async_func():
@@ -18,13 +18,13 @@ async def async_func1():
 async def on_ready():
     print(f'We have logged in as {client.user}')
 @client.event
-async def on_message(message)
+async def on_message(message):
  if message.content.startswith('start'):
   while True:
-  if n > i:
-   asyncio.run(async_func())
-   await channel.send(file=discord.File('screen.png'))
-   asyncio.run(async_func1())
-  else:
-   asyncio.sleep(0.5)
+   if n > i:
+    asyncio.run(async_func())
+    await channel.send(file=discord.File('screen.png'))
+    asyncio.run(async_func1())
+   else:
+    asyncio.sleep(0.5)
 client.run(token)
