87fe169e0fa2e5187470e000f014bdaaa81b8c1c
theoneslayingadam <125176446+theoneslayingadam@users.noreply.github.com>
2023-02-24 19:44:10 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
-apt install python3-pip -y
+apt install python3-pip nano -y
 pip install nest_asyncio
 pip install discord.py
 wget https://raw.githubusercontent.com/theoneslayingadam/repo/main/test.py
