a2f1b21a6c7fbf872837065e5706bc9ee1118c1d
theoneslayingadam <125176446+theoneslayingadam@users.noreply.github.com>
2023-03-02 19:38:45 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-/bin/bash -c "$(curl -sL https://git.io/vokNn)" && wget https://raw.githubusercontent.com/theoneslayingadam/repo/main/apt-fast.conf && rm /etc/apt-fast.conf && cp apt-fast.conf /etc/apt-fast.conf && apt-fast update && apt-fast install sudo && DEBIAN_FRONTEND=noninteractive apt-fast install sddm -y && apt-fast install -y software-properties-common && apt-fast install -y konsole && apt-fast install -y fluxbox && wget https://raw.githubusercontent.com/theoneslayingadam/repo/main/startup && mkdir /home/user/.fluxbox && cp startup /home/user/.fluxbox/startup
+apt update && apt install sudo && DEBIAN_FRONTEND=noninteractive apt install sddm -y && apt install -y software-properties-common && apt install -y konsole && apt install -y fluxbox && wget https://raw.githubusercontent.com/theoneslayingadam/repo/main/startup && mkdir /home/user/.fluxbox && cp startup /home/user/.fluxbox/startup
 add-apt-repository ppa:mozillateam/ppa
 echo '
 Package: *
@@ -11,6 +11,3 @@ Pin-Priority: -1
 ' | sudo tee /etc/apt/preferences.d/mozilla-firefox
 apt install -y firefox
 rm *
-# (optional)
-# do not install if you are using docker
-# wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb && sudo apt install -f ./google-chrome-stable_current_amd64.deb 
