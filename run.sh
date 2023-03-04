2c30daaaa677d8362f1b1968f7f79a3d26b3b406
theoneslayingadam <125176446+theoneslayingadam@users.noreply.github.com>
2023-03-04 11:52:05 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 apt update && apt-get update && apt-get install tigervnc-scraping-server git -y  && apt install sudo xvfb screen python3-pip -y && apt install -y software-properties-common && apt install -y konsole && apt install -y fluxbox && wget https://raw.githubusercontent.com/theoneslayingadam/repo/main/startup && mkdir /home/user/.fluxbox && cp startup /home/user/.fluxbox/startup
 screen -S xvfb -d -m bash -c "xvfb-run -f /root/.Xauthority -n 1 startfluxbox"
-echo -e "passwd\npasswd\nn"  | passwd
+echo -e "passwd\npasswd\nn"  | vncpasswd
 x0vncserver -passwordfile ~/.vnc/passwd -display :1
 pip install lastversion
 lastversion download https://github.com/novnc/noVNC -o novnc.tar.gz
